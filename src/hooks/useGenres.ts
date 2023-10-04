e4fbe6b1074187fd49de8d00e17a78d93602de9d
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-05 00:10:51 +0500

    Create a generic data fetching hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "axios";
-import apiClient from "../services/api-client";
+//  
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
     id: number;
     name: string
 }
-interface FetchGamesResponse {
-    count: number;
-    results: Genre[]
-}
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false)
+// interface FetchGamesResponse {
+//     count: number;
+//     results: Genre[]
+// }
+const useGenres = () => useData<Genre>('/genres');
+// const useGenres = () => {
+//   const [genres, setGenres] = useState<Genre[]>([]);
+//   const [error, setError] = useState("");
+//   const [isLoading, setLoading] = useState(false)
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true)
-    apiClient
-      .get<FetchGamesResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
+//   useEffect(() => {
+//     const controller = new AbortController();
+//     setLoading(true)
+//     apiClient
+//       .get<FetchGamesResponse>("/genres", { signal: controller.signal })
+//       .then((res) => {
+//         setGenres(res.data.results);
+//         setLoading(false);
+//       })
+//       .catch((err) => {
+//         if (err instanceof CanceledError) return;
+//         setError(err.message);
+//         setLoading(false);
+//       });
 
-    return () => controller.abort();
-  }, []);
+//     return () => controller.abort();
+//   }, []);
 
-  return { genres, error ,isLoading};
-}
+//   return { genres, error ,isLoading};
+// }
 
 export default useGenres
 
